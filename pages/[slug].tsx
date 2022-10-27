9c26a9b339bb81d2dac25d8512ddf4e796072d99
Minsu Kim <alstn2468_@naver.com>
2022-10-27 14:16:30 +0900

    Feat: Add translation data

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -4,6 +4,8 @@ import { pipe } from 'fp-ts/lib/function';
 import { isRight } from 'fp-ts/lib/Either';
 import { type ParsedUrlQuery } from 'querystring';
 import { type GetStaticProps, type GetStaticPaths } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 import {
   getFileNames,
@@ -11,6 +13,7 @@ import {
   readFileByPredicate,
 } from '~/scripts/files';
 import { useTranslation } from '~/context/L10nContext';
+import styles from '~/styles/Home.module.css';
 
 interface Params extends ParsedUrlQuery {
   slug: string,
@@ -23,9 +26,70 @@ interface Props {
 
 const TranslatedPage = (props: Props) => {
   const t = useTranslation();
-  console.log(props);
   return (
-    <Fragment>{t('hello')}</Fragment>
+    <div className={styles.container}>
+      <Head>
+        <title>{t('og-title')}</title>
+        <meta name="description" content={t('og-description')} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1
+          className={styles.title}
+          dangerouslySetInnerHTML={{ __html: t('title') }}
+        >
+        </h1>
+
+        <p className={styles.description}>
+          {t('description')}{' '}
+          <code className={styles.code}>{t('page-file-path')}</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>{t('documentation-title')} &rarr;</h2>
+            <p>{t('documentation-description')}</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>{t('learn-title')} &rarr;</h2>
+            <p>{t('learn-description')}</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>{t('examples-title')} &rarr;</h2>
+            <p>{t('examples-description')}</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.card}
+          >
+            <h2>{t('deploy-title')} &rarr;</h2>
+            <p>{t('deploy-description')}</p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('footer-powered-by')}{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
   );
 };
 
